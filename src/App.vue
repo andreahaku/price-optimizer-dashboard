ba557ad79fa18dc698948a3629aebc79f473c172
Andrea Salvatore <andrea.salvatore@consensys.net>
2025-04-25 23:24:22 +0200

    chore: added TailwindCSS support

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <h1 class="text-3xl text-blue-600 font-bold font-sans bg-yellow-100 p-4 rounded">
+      Hello world!
+    </h1>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
